8549f2b0f0f76d2751f17da2e5e8155651ad73ea
bilalfarid-1 <bilaldrosh1@gmail.com>
2025-04-29 10:56:36 +0500

    add customization btn to food-item and fix the cart section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+const cart = document.querySelector('#cart-section');
+cart.style.display = 'none';
 document.querySelector('.btn-cart').addEventListener('click', function() {
-    const cart = document.querySelector('#cart-section');
+    
     if (cart.style.display === 'none' || cart.style.display === '') {
         cart.style.display = 'block';
     } else {
